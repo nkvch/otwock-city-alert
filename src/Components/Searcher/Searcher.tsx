4ee43afa07be60b4109ecd42f5b7872a8e388a9a
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-06-18 08:54:14 +0200

    only to the right

--- src/Components/Searcher/Searcher.tsx
+++ src/Components/Searcher/Searcher.tsx
@@ -16,7 +16,6 @@ const StyledAutocomplete = styled(Autocomplete)`
       padding-left: 1rem !important;
     }
   }
-
 `;
 
 export interface SearcherProps {
@@ -27,38 +26,39 @@ function Searcher(props: SearcherProps) {
   const { onSelectLocation } = props;
   const [searchValue, setSearchValue] = useState<string>("");
 
-  const {
-    data,
-    isFetching,
-  } = useQuery(
-    ["locations", searchValue],
-    () => searchOpenStreetMap({
+  const { data, isFetching } = useQuery(["locations", searchValue], () =>
+    searchOpenStreetMap({
       q: searchValue,
-    }),
+    })
   );
 
-  const onSelectOption = useCallback((event: any, value: unknown) => {
-    if (value) {
-      const location = data?.find((location) => location.display_name === value);
+  const onSelectOption = useCallback(
+    (event: any, value: unknown) => {
+      if (value) {
+        const location = data?.find(
+          (location) => location.display_name === value
+        );
 
-      if (location) {
-        setSearchValue("");
-        onSelectLocation(location);
+        if (location) {
+          setSearchValue("");
+          onSelectLocation(location);
+        }
       }
-    }
-  }, [data, onSelectLocation]);
+    },
+    [data, onSelectLocation]
+  );
 
   return (
     <StyledAutocomplete
+      
       loading={isFetching}
       id="free-solo-2-demo"
-      options={
-        data?.map((location) => location.display_name) || []
-      }
+      options={data?.map((location) => location.display_name) || []}
       value={searchValue}
       onChange={onSelectOption}
       renderInput={(params) => (
         <TextField
+        className="target-for-scroll"
           {...params}
           label="Szukaj..."
           onChange={(event) => setSearchValue(event.target.value)}
